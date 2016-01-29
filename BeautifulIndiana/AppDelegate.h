ab2cb8cfea44251772980088b14520c9a9f54118
Helios001 <kuye324@163.com>
2016-01-29 18:31:13 +0800

    Initial Commit

---
+++ BeautifulIndiana/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeautifulIndiana
+//
+//  Created by Helios on 16/1/29.
+//  Copyright © 2016年 Helios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeautifulIndiana/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeautifulIndiana
+//
+//  Created by Helios on 16/1/29.
+//  Copyright © 2016年 Helios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
